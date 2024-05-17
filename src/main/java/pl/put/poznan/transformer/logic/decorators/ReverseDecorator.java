364029ff5337443bd641ced5a5bd2e26801b51bc
WojtekNagorka <150377892+WojtekNagorka@users.noreply.github.com>
2024-05-17 20:45:51 +0200

    adding documentation to ReverseDecorator

--- src/main/java/pl/put/poznan/transformer/logic/decorators/ReverseDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/ReverseDecorator.java
@@ -2,12 +2,25 @@ package pl.put.poznan.transformer.logic.decorators;
 
 import pl.put.poznan.transformer.logic.TextTransformation;
 
+/**
+ * This class is a concrete decorator used to reverse the input text
+ */
 public class ReverseDecorator extends TransformationDecorator {
 
+    /**
+     * ReverseDecorator constructor
+     * @param textTransformation object to be wrapped
+     */
     public ReverseDecorator(TextTransformation textTransformation) {
         super(textTransformation);
     }
 
+    /**
+     * This method is used to reverse the given text. Given an input string, it returns a
+     * string in reverse
+     * @param text string input by the user
+     * @return reversed text
+     */
     public String transform(String text) {
         StringBuilder builder = new StringBuilder(text);
         String result = builder.reverse().toString();
