50bd94fd8256a803ed9721899be5c458dbfc2ceb
PBalewski <balewskipiotrek@gmail.com>
2024-05-18 21:46:13 +0200

    AcronymExpansion that breaks format but works

--- src/main/java/pl/put/poznan/transformer/logic/decorators/AcronymCompressionDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorators/AcronymCompressionDecorator.java
@@ -1,12 +1,12 @@
 package pl.put.poznan.transformer.logic.decorators;
 
 import pl.put.poznan.transformer.logic.TextTransformation;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+//import java.util.ArrayList; import for format handling
+//import java.util.List;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
 
 
 public class AcronymCompressionDecorator extends TransformationDecorator{
@@ -24,16 +24,17 @@ public class AcronymCompressionDecorator extends TransformationDecorator{
     @Override
     public String transform(String text) {
         StringBuilder builder = new StringBuilder();
-        Pattern pattern = Pattern.compile("\\s+");
-        Matcher matcher = pattern.matcher(text);
-        List<String> whitespaces = new ArrayList<>();
-        while (matcher.find()) {
-            whitespaces.add(matcher.group());
-        }
+//        I Have comment lines that were used for format handling which unfortunately don't work
+//        Pattern pattern = Pattern.compile("\\s");
+//        Matcher matcher = pattern.matcher(text);
+//        List<String> whitespaces = new ArrayList<>();
+//        while (matcher.find()) {
+//            whitespaces.add(matcher.group());
+//        }
 
         String[] words = text.split("(?<=\\s)(?=\\s)|\\s+");
 
-        int i = 0;
+//        int i = 0;
         int omitWords = 0;
 
         for (int j = 0; j < words.length; j++) {
@@ -45,43 +46,50 @@ public class AcronymCompressionDecorator extends TransformationDecorator{
             String Word1 = words[j];
             String Word2 = (j + 1 < words.length) ? words[j + 1] : "";
             String Word3 = (j + 2 < words.length) ? words[j + 2] : "";
-            String whitespace1 = "";
-            String whitespace2 = "";
-            if (i < whitespaces.size()) {
-                whitespace1 = whitespaces.get(i);
-            }
-            if (i < whitespaces.size()-1) {
-                whitespace2 = whitespaces.get(i+1);
-            }
+//            String whitespace1 = "";
+//            String whitespace2 = "";
+//            if (i < whitespaces.size()) {
+//                whitespace1 = whitespaces.get(i);
+//            }
+//            if (i < whitespaces.size()-1) {
+//                whitespace2 = whitespaces.get(i+1);
+//            }
 
-            String Words12 = Word1 + whitespace1 + Word2;
-            String Words23 = Word2 + whitespace2 + Word3;
-            String Words123 = Words12 + whitespace2 + Word3;
+            String Words12 = Word1 + " " + Word2;
+            String Words23 = Word2 + " " + Word3;
+            String Words123 = Words12 + " " + Word3;
             String lowerWords12 = Words12.toLowerCase();
             String lowerWords23 = Words23.toLowerCase();
             String lowerWords123 = Words123.toLowerCase();
 
+
             if (ABBREVIATION_MAP.containsKey(lowerWords12)) {
                 String shortForm = ABBREVIATION_MAP.get(lowerWords12);
                 builder.append(shortForm);
-                builder.append(whitespace2);
+//                builder.append(whitespace2);
+                builder.append(" "); // previous line instead of this one
                 omitWords = 1;
-                ++i;
+//                ++i;
             } else if (ABBREVIATION_MAP.containsKey(lowerWords23)) {
                 builder.append(Word1);
-                builder.append(whitespace1);
+//                builder.append(whitespace1);
+                builder.append(" "); // previous line instead of this one
                 String shortForm = ABBREVIATION_MAP.get(lowerWords23);
                 builder.append(shortForm);
+                builder.append(" "); // redundant in formating version
                 omitWords = 2;
-                ++i;
+//                ++i;
             } else if (ABBREVIATION_MAP.containsKey(lowerWords123)) {
-                builder.append(lowerWords123);
+                String shortForm = ABBREVIATION_MAP.get(lowerWords123);
+                builder.append(shortForm);
+                builder.append(" "); // redundant in formating version
                 omitWords = 2;
-                i+=2;
+//                i+=2;
             } else {
                 builder.append(Word1);
-                builder.append(whitespace1);
-                ++i;
+//                builder.append(whitespace1);
+                builder.append(" "); //previous line instead of this one
+//                ++i;
             }
         }
 
